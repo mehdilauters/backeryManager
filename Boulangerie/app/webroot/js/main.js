736ecc910716c089435db85057438c99d87dd02e
mehdilauters <mehdilauters@gmail.com>
2013-12-30 09:47:29 +0000

    App

---
+++ Boulangerie/app/webroot/js/main.js
@@ -0,0 +1,41 @@
+
+
+$(document).ready(function(){
+    $('.hideJs').each(function(index, item){
+	$(item).hide();
+      });
+      
+    $('.showJs').each(function(index, item){
+	$(item).show();
+      });
+    
+    
+      $.datepicker.setDefaults($.datepicker.regional[ 'fr' ]);
+      $( ".datepicker" ).datepicker();
+    
+    
+  setInterval(function(){  
+         $(".slideshow ul").animate({marginLeft:-350},800,function(){  
+            $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));  
+         })  
+      }, 3500);  
+    
+    
+    $(".fancybox").fancybox(
+        {
+            type: "image",
+            //autoDimensions  : true,
+            //width             : 600,
+            //height            : 'auto',
+            showNavArrows: true,
+	    beforeLoad: function() {
+	      this.title = '<span class="fancybox-title">' + $(this.element).find('img').attr('alt') + '<span class="fancybox-title-count"> ( ' + (this.index + 1) + ' / ' + this.group.length + ' )</span>';
+	    },
+        }
+          );
+    
+    
+    
+    
+    
+  }); 
